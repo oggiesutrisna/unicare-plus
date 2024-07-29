6120ae34bb62f6a577097b23b4ee7d73a82903b7
oggiesutrisna <sutrisna.oggie@gmail.com>
2024-07-29 14:31:33 +0800

    repairing all of these projects and make it awesome

--- config/database.php
+++ config/database.php
@@ -86,7 +86,7 @@ return [
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'postgres'),
             'username' => env('DB_USERNAME', 'postgres.qmvtvpelsyemhepiqyte'),
-            'password' => env('DB_PASSWORD', 'VUfw92pdkeMy5xJh'),
+            'password' => env('DB_PASSWORD', 'WT7MGbJ2gaCDhsym'),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
